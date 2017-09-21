4abbbf7a5a5d3f96f87eb150be93b36646a93389
Arunkumar <frapin@outlook.com>
2017-09-21 06:15:12 +0530

    render using react

--- sampleone/src/App.js
+++ sampleone/src/App.js
@@ -1,20 +1,56 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
+  constructor(props)
+  {
+    super(props);
+    this.state = {compareUsingColour:false};
+
+    this.getCells = this.getCells.bind(this);
+    this.handleRunClick = this.handleRunClick.bind(this);
+  }
+
+  getCells = () => {
+    const cells = window.sample75kdata.map((item) => {
+      if(this.state.compareUsingColour){
+        let colourClass = '';
+        if(item.value > 5.5){
+          colourClass = 'bg-success';
+        }
+        else if (item.value > 4.5 && item.value <5.5)
+        {
+          colourClass = 'bg-info';
+        }
+        else
+        {
+          colourClass = 'bg-danger';
+        }
+        return <span key={item._id} className={"cell reactcell " + colourClass}> {item.value} </span>;
+      }
+      else
+      {
+        return <span key={item._id} className="cell reactcell "> {item.value} </span>; 
+      }
+    }); 
+    return cells;
+  }
+  
+  handleRunClick = () => {
+    this.setState({compareUsingColour:true});
+  }
+
   render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    const app = <div className="App">
+                  <button id="reactrun" onClick={this.handleRunClick} type="button" className="btn btn-primary">Run Using React</button>
+                  <div className="row">
+                      <div className="col-md-12">
+                          <div id="reactoutput">
+                            {this.getCells()}
+                          </div>
+                      </div>
+                </div>
+                </div>;
+    return app;
   }
 }
 
